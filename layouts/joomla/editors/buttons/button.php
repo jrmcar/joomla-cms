c08527aee7db6a235b312db24c62abca891d269b
dgt41 <d.grammatiko@gmail.com>
2014-10-14 07:53:46 +0300

    Move editor plugin butoons modals to Bootstrap

--- layouts/joomla/editors/buttons/button.php
+++ layouts/joomla/editors/buttons/button.php
@@ -11,16 +11,24 @@ defined('_JEXEC') or die;
 
 $button = $displayData;
 
+if ($button->get('name'))
+{
+	$class    = ($button->get('class')) ? $button->get('class') : null;
+	$class   .= ($button->get('modal')) ? ' modal-button' : null;
+	$href     = ($button->get('link')) ? ' href="' . JUri::base() . $button->get('link') . '"' : null;
+	$onclick  = ($button->get('onclick')) ? ' onclick="' . $button->get('onclick') . '"' : ' onclick="IeCursorFix(); return false;"';
+	$title    = ($button->get('title')) ? $button->get('title') : $button->get('text');
+
+	if ($button->get('modal'))
+	{
+		// Load modal popup behavior
+		JHtml::_('behavior.modal', 'a.modal-button');
+	}
 ?>
-<?php if ($button->get('name')) : ?>
-	<?php
-		$class    = ($button->get('class')) ? $button->get('class') : null;
-		$class	 .= ($button->get('modal')) ? ' modal-button' : null;
-		$href     = ($button->get('link')) ? ' href="' . JUri::base() . $button->get('link') . '"' : null;
-		$onclick  = ($button->get('onclick')) ? ' onclick="' . $button->get('onclick') . '"' : ' onclick="IeCursorFix(); return false;"';
-		$title    = ($button->get('title')) ? $button->get('title') : $button->get('text');
-	?>
-	<a class="<?php echo $class; ?>" title="<?php echo $title; ?>" <?php echo $href . $onclick; ?> rel="<?php echo $button->get('options'); ?>">
-		<i class="icon-<?php echo $button->get('name'); ?>"></i> <?php echo $button->get('text'); ?>
-	</a>
-<?php endif;
+
+<a class="<?php echo $class; ?>" title="<?php echo $title; ?>" <?php echo $href . $onclick; ?> rel="<?php echo $button->get('options'); ?>">
+	<i class="icon-<?php echo $button->get('name'); ?>"></i> <?php echo $button->get('text'); ?>
+</a>
+
+<?php
+}
