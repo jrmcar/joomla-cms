83406ad4c2eec3e574c8d3626b5b619038938d4f
dgt41 <d.grammatiko@gmail.com>
2015-02-06 15:41:40 +0200

    Formvalidator + jquery template.js

--- administrator/templates/hathor/html/com_modules/module/edit.php
+++ administrator/templates/hathor/html/com_modules/module/edit.php
@@ -11,13 +11,13 @@ defined('_JEXEC') or die;
 
 JHtml::addIncludePath(JPATH_COMPONENT.'/helpers/html');
 
-JHtml::_('behavior.formvalidation');
+JHtml::_('behavior.formvalidator');
 JHtml::_('behavior.combobox');
 $hasContent = empty($this->item->module) || $this->item->module == 'custom' || $this->item->module == 'mod_custom';
 
 $script = "Joomla.submitbutton = function(task)
 	{
-			if (task == 'module.cancel' || document.formvalidator.isValid(document.id('module-form'))) {";
+			if (task == 'module.cancel' || document.formvalidator.isValid(document.getElementById('module-form'))) {";
 if ($hasContent)
 {
 	$script .= $this->form->getField('content')->save();
